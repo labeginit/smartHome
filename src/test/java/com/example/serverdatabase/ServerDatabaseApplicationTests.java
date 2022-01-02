7bf92217bffd6c574dccc4ae9a17a7a163a4bf11
liliia <liliyameister@gmail.com>
2022-01-02 18:47:59 +0100

    added tests to cover all API methods

--- src/test/java/com/example/serverdatabase/ServerDatabaseApplicationTests.java
+++ src/test/java/com/example/serverdatabase/ServerDatabaseApplicationTests.java
@@ -1,54 +1,214 @@
 package com.example.serverdatabase;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Java6Assertions.*;
 import java.util.HashMap;
 
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 class ServerDatabaseApplicationTests {
     WebSocketHandler socketHandler = new WebSocketHandler();
-/*
-    @Test
-    void getDevices() {
-        String actual = socketHandler.getDeviceStatuses();
-        System.out.println(actual);
-        assertThat(actual).isNotNull();
-    }
-*/
 
     @Test
-    void changeTVStatus(){
+    @Order(1)
+    void changeStatusTV() {
         String message = "{'_id':'Bedroom TV', 'status':'true'}";
         HashMap response = socketHandler.changeDeviceStatus(message);
-        assertThat(response.get("message")).isEqualTo(message);
+        assert(response.get("message")).equals(message);
+    }
+/*
+    @Test
+    @Order(2)
+    void getDevices() {
+        String actual = socketHandler.getDeviceStatuses();
+        assert(actual != null);
     }
 
-/*
+    @Test
+    @Order(3)
+    void addThermo() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.addDevice("{'_id':'Test Thermometer',device:'thermometer','status':'20'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
 
     @Test
-    void changeLampStatus(){
+    @Order(3)
+    void addLamp() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.addDevice("{'_id':'Test Lamp',device:'lamp','status':'false'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(3)
+    void addAlarm() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.addDevice("{'_id':'Test Alarm',device:'alarm','status':'0'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(3)
+    void addHeater() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.addDevice("{'_id':'Test Heater',device:'heater','status':'false'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(3)
+    void addFan() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.addDevice("{'_id':'Test Fan',device:'fan','status':'0'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(4)
+    void removeThermo() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.removeDevice("{'_id':'Test Thermometer'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(4)
+    void removeLamp() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.removeDevice("{'_id':'Test Lamp'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(4)
+    void removeAlarm() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.removeDevice("{'_id':'Test Alarm'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(4)
+    void removeHeater() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.removeDevice("{'_id':'Test Heater'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(4)
+    void removeFan() {
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.removeDevice("{'_id':'Test Fan'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(5)
+    void changeStatusLamp() {
         HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'Outdoor lamp', 'status':'true'}");
         HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'Outdoor lamp', 'status':'false'}");
-        assertThat(twiceChange).isNotEqualTo(onceChange);
+        assert(!twiceChange.equals(onceChange));
     }
 
     @Test
-    void changeFanStatus(){
-        HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'Bedroom Fan', 'status':'0'}");
-        HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'Bedroom Fan', 'status':'3'}");
-        assertThat(twiceChange).isNotEqualTo(onceChange);
+    @Order(5)
+    void changeStatusFan() {
+        HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'Bedroom Fan', 'status':'4'}");
+        HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'Bedroom Fan', 'status':'0'}");
+        assert(!twiceChange.equals(onceChange));
     }
 
     @Test
-    void changeThermoStatus(){
+    @Order(5)
+    void changeStatusThermo() {
         HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'Livingroom Thermometer', 'status':'20.4'}");
         HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'Livingroom Thermometer', 'status':'25'}");
-        System.out.println(socketHandler.getDeviceStatuses());
-        assertThat(twiceChange).isNotEqualTo(onceChange);
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(5)
+    void changeStatusAlarm() {
+        HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'alarm', 'status':'1'}");
+        HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'alarm', 'status':'0'}");
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(5)
+    void changeStatusHeater() {
+        HashMap onceChange = socketHandler.changeDeviceStatus("{'_id':'House Heater', 'status':'true'}");
+        HashMap twiceChange = socketHandler.changeDeviceStatus("{'_id':'House Heater', 'status':'false'}");
+        assert(!twiceChange.equals(onceChange));
+    }
+
+
+    @Test
+    @Order(6)
+    void getConfirmationLamp() {
+        socketHandler.getConfirmation("{'_id':'Indoor lamp',device:'lamp','status':'true', result:'success'}");
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.getConfirmation("{'_id':'Indoor lamp',device:'lamp','status':'false', result:'success'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(6)
+    void getConfirmationAlarm() {
+        socketHandler.getConfirmation("{'_id':'alarm',device:'alarm','status':'1', result:'success'}");
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.getConfirmation("{'_id':'alarm',device:'alarm','status':'0', result:'success'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(6)
+    void getConfirmationFan() {
+        socketHandler.getConfirmation("{'_id':'Bedroom Fan',device:'fan','status':'3', result:'success'}");
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.getConfirmation("{'_id':'Bedroom Fan',device:'fan','status':'0', result:'success'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(6)
+    void getConfirmationHeater() {
+        socketHandler.getConfirmation("{'_id':'House Heater',device:'heater','status':'true', result:'success'}");
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.getConfirmation("{'_id':'House Heater',device:'heater','status':'false', result:'success'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
+    }
+
+    @Test
+    @Order(6)
+    void getConfirmationThermo() {
+        socketHandler.getConfirmation("{'_id':'Livingroom Thermometer',device:'thermometer','status':'20', result:'success'}");
+        String onceChange = socketHandler.getDeviceStatuses();
+        socketHandler.getConfirmation("{'_id':'Livingroom Thermometer',device:'thermometer','status':'18', result:'success'}");
+        String twiceChange = socketHandler.getDeviceStatuses();
+        assert(!twiceChange.equals(onceChange));
     }
 */
 }
